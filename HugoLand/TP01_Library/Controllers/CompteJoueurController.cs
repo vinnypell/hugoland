b250439a2d697a88ba727510ca89421a3232eec0
MathiasLavoie-Rivard <70145948+MathiasLavoie-Rivard@users.noreply.github.com>
2021-02-13 15:29:00 -0500

    Modification de controllers

--- HugoLand/TP01_Library/Controllers/CompteJoueurController.cs
+++ HugoLand/TP01_Library/Controllers/CompteJoueurController.cs
@@ -8,5 +8,96 @@ namespace TP01_Library
 {
     public class CompteJoueurController
     {
+
+
+        /// <summary>
+        /// Auteur: Mathias Lavoie-Rivard |
+        /// Summary: Permet de créer un compte. |
+        /// Date: 2021-02-11
+        /// </summary>
+        /// <param name="p_NomJoueur"></param>
+        /// <param name="p_Courriel"></param>
+        /// <param name="p_Prenom"></param>
+        /// <param name="p_Nom"></param>
+        /// <param name="p_TypeUtilisateur"></param>
+        /// <param name="p_Mdp"></param>
+        /// <returns>Message de succès ou d'erreur</returns>
+        public string CreerJoueur(string p_NomJoueur, string p_Courriel, string p_Prenom,string p_Nom, int p_TypeUtilisateur, string p_Mdp)
+        {
+            System.Data.Entity.Core.Objects.ObjectParameter myOutputParamString = new System.Data.Entity.Core.Objects.ObjectParameter("myOutputParamString", typeof(string));
+            using (HugoLandContext dbcontext = new HugoLandContext())
+            {
+                dbcontext.CreerCompteJoueur(p_NomJoueur, p_Courriel, p_Prenom, p_Nom, p_TypeUtilisateur,p_Mdp, myOutputParamString);
+            }
+            return (Convert.ToString(myOutputParamString.Value));
+        }
+        /// <summary>
+        /// Auteur: Mathias Lavoie-Rivard | 
+        /// Summary: Permet de supprimer un compte. | 
+        /// Date: 2021-02-11
+        /// </summary>
+        /// <param name="p_CompteJoueur"></param>
+        public void SupprimerJoueur(CompteJoueur p_CompteJoueur)
+        {
+            using (HugoLandContext dbcontext = new HugoLandContext())
+            {
+                dbcontext.CompteJoueurs.Remove(p_CompteJoueur);
+                dbcontext.SaveChanges();
+            }
+        }
+        /// <summary>
+        /// Auteur: Mathias Lavoie-Rivard | 
+        /// Summary: Permet de modifier un compte. | 
+        /// Date: 2021-02-11
+        /// </summary>
+        /// <param name="p_CompteJoueur"></param>
+        public void ModifierJoueur(CompteJoueur p_CompteJoueur, string p_NomJoueur = null, string p_Courriel = null, string p_Prenom = null, string p_Nom = null, int? p_TypeUtilisateur = null)
+        {
+            //TODO AJOUTER LES INFORAMTION AU COMPTE ET VÉRIFIER SI L'INFORMATION EST CHANGÉ OU NON.
+            using (HugoLandContext dbContext = new HugoLandContext())
+            {
+                CompteJoueur joueurModif = dbContext.CompteJoueurs.FirstOrDefault(x => x.Id == p_CompteJoueur.Id);
+
+                if (string.IsNullOrEmpty(p_NomJoueur))
+                {
+                joueurModif.NomJoueur = p_NomJoueur;
+                }
+                if (string.IsNullOrEmpty(p_Courriel))
+                {
+                    joueurModif.Courriel = p_Courriel;
+                }
+                if (string.IsNullOrEmpty(p_Prenom))
+                {
+                    joueurModif.Prenom = p_Prenom;
+                }
+                if (string.IsNullOrEmpty(p_Nom))
+                {
+                    joueurModif.Nom = p_Nom;
+                }
+                if (p_TypeUtilisateur == null)
+                {
+                    joueurModif.TypeUtilisateur = (int)p_TypeUtilisateur;
+                }
+
+                dbContext.SaveChanges();
+            }
+        }
+        /// <summary>
+        /// Auteur: Mathias Lavoie-Rivard | 
+        /// Summary: Permet de Valider la connexion du joueur | 
+        /// Date: 2021-02-11
+        /// </summary>
+        /// <param name="p_Mdp"></param>
+        /// <param name="p_NomJoueur"></param>
+        /// <returns>Message de succès ou d'erreur</returns>
+        public string ValiderConnexion(string p_Mdp, string p_NomJoueur)
+        {
+            System.Data.Entity.Core.Objects.ObjectParameter myOutputParamString = new System.Data.Entity.Core.Objects.ObjectParameter("myOutputParamString", typeof(string));
+            using (HugoLandContext dbcontext = new HugoLandContext())
+            {
+                dbcontext.Connexion(p_NomJoueur,p_Mdp,myOutputParamString);
+            }
+            return (Convert.ToString(myOutputParamString.Value));
+        }
     }
 }
--- HugoLand/TP01_Library/Controllers/ItemController.cs
+++ HugoLand/TP01_Library/Controllers/ItemController.cs
@@ -8,5 +8,60 @@ namespace TP01_Library
 {
     public class ItemController
     {
+        /// <summary>
+        /// Auteur: Mathias Lavoie-Rivard | 
+        /// Summary: Permet de créer un nouvel item. | 
+        /// Date: 2021-02-11
+        /// </summary>
+        /// <param name="p_nom"></param>
+        /// <param name="p_description"></param>
+        /// <param name="p_x"></param>
+        /// <param name="p_y"></param>
+        /// <param name="p_ImageID"></param>
+        /// <param name="p_MondeID"></param>
+        public void AjouterItems(string p_nom, string p_description, int p_x, int p_y, int p_ImageID, int p_MondeID)
+        {
+            using (HugoLandContext dbcontext = new HugoLandContext())
+            {
+                dbcontext.Items.Add(new Item()
+                {
+                    Nom = p_nom,
+                    Description = p_description,
+                    x = p_x,
+                    y = p_y,
+                    MondeId = p_MondeID,
+                });
+
+                dbcontext.SaveChanges();
+            }
+        }
+        /// <summary>
+        /// Auteur: Mathias Lavoie-Rivard | 
+        /// Summary: Permet de supprimer un item de la map et de l'ajouter dans l'inventaire d'un hero | 
+        /// Date: 2021-02-11
+        /// </summary>
+        /// <param name="p_Item"></param>
+        /// <param name="p_CompteJoeur"></param>
+        public void SupprimerItem(Item p_Item, Hero p_Hero)
+        {
+            using (HugoLandContext dbcontext = new HugoLandContext())
+            {
+                Item itemDelete = dbcontext.Items.FirstOrDefault(x => x.Id == p_Item.Id);
+
+                itemDelete.x = null;
+                itemDelete.y = null;
+                itemDelete.IdHero = p_Hero.Id;
+                dbcontext.SaveChanges();
+            }
+        }
+        /// <summary>
+        /// Auteur: Mathias Lavoie-Rivard | 
+        /// Summary: Permet de modifier la quantité d'item qu'un hero a | 
+        /// Date: 2021-02-11
+        /// </summary>
+        public void ModifierQuantiteItem()
+        {
+
+        }
     }
 }
