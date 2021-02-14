07a1fe0fcb4ba579dae3f158a98e7c68c84e2321
SLalancette <slalan7@hotmail.fr>
2021-02-13 21:55:44 -0500

    setup test herocontroller

--- HugoLand/TP01_Tests/Controllers/HeroControllerTests.cs
+++ HugoLand/TP01_Tests/Controllers/HeroControllerTests.cs
@@ -5,15 +5,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
 
 namespace TP01_Library.Tests.Controllers
 {
     [TestClass()]
     public class HeroControllerTests
     {
+        HeroController controller = new HeroController();
+        //Mock le dbContext
+        Mock<HugoLandContext> context = new Mock<HugoLandContext>();
+
         [TestMethod()]
         public void AjouterHeroTest()
         {
+            #region Arrange
+            // variables
+            context.
+            #endregion
+
+            #region Act
+            // call de la méthode à testé
+            #endregion
+
+            #region Assert
+            // Assert.IsTrue(valeur1, valeur2);
+            #endregion
+
             Assert.Fail();
         }
 
