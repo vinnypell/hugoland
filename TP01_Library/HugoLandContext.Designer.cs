1c31063d16e43b92a1ffec1abdd56017874b8b43
MathiasLavoie-Rivard <70145948+MathiasLavoie-Rivard@users.noreply.github.com>
2021-03-05 19:21:18 -0500

    Changement du entityframekwork

--- TP01_Library/HugoLandContext.Designer.cs
+++ TP01_Library/HugoLandContext.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\01_cégep\session-4\dev-bdd\travaux\hugoland\TP01_Library\HugoLandContext.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\matla\Documents\GitHub\hugoland\TP01_Library\HugoLandContext.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
