97904ce079df0a5a210da847a6a178ee9e9cc174
MathiasLavoie-Rivard <70145948+MathiasLavoie-Rivard@users.noreply.github.com>
2021-03-05 19:30:01 -0500

    Ajout page de connexion

---
+++ TP02 - HugoLand - Editeur de Mondes (Base)/Presentation/frmConnection.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace HugoLandEditeur.Presentation
+{
+    partial class frmConnection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_username = new System.Windows.Forms.Label();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.lbl_Password = new System.Windows.Forms.Label();
+            this.btn_connection = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_username
+            // 
+            this.lbl_username.AutoSize = true;
+            this.lbl_username.Location = new System.Drawing.Point(12, 9);
+            this.lbl_username.Name = "lbl_username";
+            this.lbl_username.Size = new System.Drawing.Size(73, 17);
+            this.lbl_username.TabIndex = 0;
+            this.lbl_username.Text = "Username";
+            // 
+            // txt_username
+            // 
+            this.txt_username.Location = new System.Drawing.Point(15, 29);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(148, 22);
+            this.txt_username.TabIndex = 1;
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(15, 74);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(148, 22);
+            this.txt_password.TabIndex = 2;
+            this.txt_password.TextChanged += new System.EventHandler(this.txt_password_TextChanged);
+            // 
+            // lbl_Password
+            // 
+            this.lbl_Password.AutoSize = true;
+            this.lbl_Password.Location = new System.Drawing.Point(12, 54);
+            this.lbl_Password.Name = "lbl_Password";
+            this.lbl_Password.Size = new System.Drawing.Size(69, 17);
+            this.lbl_Password.TabIndex = 3;
+            this.lbl_Password.Text = "Password";
+            // 
+            // btn_connection
+            // 
+            this.btn_connection.Location = new System.Drawing.Point(12, 120);
+            this.btn_connection.Name = "btn_connection";
+            this.btn_connection.Size = new System.Drawing.Size(151, 33);
+            this.btn_connection.TabIndex = 5;
+            this.btn_connection.Text = "Connect";
+            this.btn_connection.UseVisualStyleBackColor = true;
+            // 
+            // frmConnection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(181, 177);
+            this.Controls.Add(this.btn_connection);
+            this.Controls.Add(this.lbl_Password);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_username);
+            this.Controls.Add(this.lbl_username);
+            this.Name = "frmConnection";
+            this.Text = "Connection";
+            this.Load += new System.EventHandler(this.frmConnection_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_username;
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.Label lbl_Password;
+        private System.Windows.Forms.Button btn_connection;
+    }
+}
---
+++ TP02 - HugoLand - Editeur de Mondes (Base)/Presentation/frmConnection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HugoLandEditeur.Presentation
+{
+    public partial class frmConnection : Form
+    {
+        public frmConnection()
+        {
+            InitializeComponent();
+        }
+
+        private void txt_password_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmConnection_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
